2c27a0ffce398edcef24699fe71e629174e4881b
John Tran <john.tran@shiphawk.com>
2015-10-10 21:13:36 -0700

    Added follower model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :followers do |r|
+    collection do
+      get 'random'
+    end
+  end
   resources :tweets
   get 'profile' => 'profile#index'
   patch 'profile' => 'profile#update'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010013625) do
+ActiveRecord::Schema.define(version: 20151011040354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "followed_by"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer  "user_id"
