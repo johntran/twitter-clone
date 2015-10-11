615f3159a876436010fdbf0d23807ecaaa0d44a4
John Tran <john.tran@shiphawk.com>
2015-10-10 17:46:30 -0700

    Add gravatar

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -308,11 +308,7 @@
 	      return React.createElement(
 	        "li",
 	        { className: "collection-item avatar" },
-	        React.createElement(
-	          "i",
-	          { className: "material-icons circle" },
-	          "person_pin"
-	        ),
+	        React.createElement("img", { className: "circle", src: this.props.gravatar }),
 	        React.createElement(
 	          "span",
 	          { className: "title" },
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,11 +3,16 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
 
   def as_json(options={})
-    super(methods: [:name])
+    super(methods: [:name, :gravatar])
   end
 
   def name
     user.display_name
+  end
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(user.email)
+    "http://www.gravatar.com/avatar/#{hash}"
   end
 
 end
