1ef448253e203ab8439e568711dcd824232cb48d
John Tran <john.tran@shiphawk.com>
2015-10-10 18:35:48 -0700

    Add dispatcher and flux

---
+++ app/assets/frontend/API.jsx
@@ -0,0 +1,8 @@
+import ServerActions from "./actions/ServerActions"
+export default {
+  getAllTweets(){
+    $.get("/tweets")
+    .success( rawTweets => ServerActions.receivedTweets(rawTweets))
+    .error(error => console.log(error));
+  }
+}
